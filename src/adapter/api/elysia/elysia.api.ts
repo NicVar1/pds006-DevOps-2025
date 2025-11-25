ca32c1732b4bdec5100eb9d0b312fc6000871d26
AndresGoGutierrez <andregogutierrez@gmail.com>
2025-11-24 20:44:06 -0500

    Integration axiom telemetry

--- src/adapter/api/elysia/elysia.api.ts
+++ src/adapter/api/elysia/elysia.api.ts
@@ -2,11 +2,15 @@ import { ComputerService, DeviceService, MedicalDeviceService } from "@/core/ser
 import { Controller } from "./controller.elysia";
 
 import openapi from "@elysiajs/openapi";
-import Elysia from "elysia";
+import { Elysia } from "elysia"; // <- importante, con llaves
+
+import { opentelemetry } from "@elysiajs/opentelemetry";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 
 export class ElysiaApiAdapter {
-    private controller: Controller
-    public app: Elysia
+    private controller: Controller;
+    public app: Elysia;
 
     constructor(
         computerService: ComputerService,
@@ -17,16 +21,32 @@ export class ElysiaApiAdapter {
             computerService,
             deviceService,
             medicalDeviceService
-        )
+        );
 
         this.app = new Elysia()
+           
+            .use(
+                opentelemetry({
+                    spanProcessors: [
+                        new BatchSpanProcessor(
+                            new OTLPTraceExporter({
+                                url: "https://api.axiom.co/v1/traces",
+                                headers: {
+                                    Authorization: `Bearer ${Bun.env.AXIOM_TOKEN}`,
+                                    "X-Axiom-Dataset": Bun.env.AXIOM_DATASET ?? ""
+                                }
+                            })
+                        )
+                    ]
+                })
+            )
+            
             .use(openapi({}))
-            .use(this.controller.routes())
+            .use(this.controller.routes());
     }
 
     async run() {
-        this.app.listen(3000)
-        
-        console.log("El servidor esta corriendo en el puerto 3000")
+        this.app.listen(3000);
+        console.log("Servidor corriendo en el puerto 3000 con telemetría habilitada");
     }
 }
